682f93ced1d6c50e7dd9cdc6b2d472b9c08239f4
Johan Lindfors <johan.lindfors@coderox.se>
2019-01-30 14:29:45 +0100

    Initial commit

---
+++ src/pch.h
@@ -0,0 +1,13 @@
+﻿//
+// pch.h
+// Header for platform projection include files
+//
+
+#pragma once
+
+#include <windows.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.Xaml.Markup.h>
+#include <winrt/Windows.UI.Xaml.Input.h>
+#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
+#include "BlankUserControl.h"
